af0d0f6a8a5c919fd3ec3fe329a4fefbe6366053
SR42-dev <sriram.radhakrishna42@gmail.com>
2022-06-21 14:06:26 +0530

    Saving translation data calculation as a seperate file.

---
+++ speedOfApproach.py
@@ -0,0 +1,191 @@
+import cv2
+import math
+import time
+import mediapipe as mp
+
+class PoseDetector:
+    """
+    Estimates Pose points of a human body using the mediapipe library.
+    """
+
+    def __init__(self, mode=False, smooth=True,
+                 detectionCon=0.5, trackCon=0.5):
+        """
+        :param mode: In static mode, detection is done on each image: slower
+        :param upBody: Upper boy only flag
+        :param smooth: Smoothness Flag
+        :param detectionCon: Minimum Detection Confidence Threshold
+        :param trackCon: Minimum Tracking Confidence Threshold
+        """
+
+        self.mode = mode
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(static_image_mode=self.mode,
+                                     smooth_landmarks=self.smooth,
+                                     min_detection_confidence=self.detectionCon,
+                                     min_tracking_confidence=self.trackCon)
+
+    def findPose(self, img, draw=True):
+        """
+        Find the pose landmarks in an Image of BGR color space.
+        :param img: Image to find the pose in.
+        :param draw: Flag to draw the output on the image.
+        :return: Image with or without drawings
+        """
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, draw=True, bboxWithHands=False):
+        self.lmList = []
+        self.bboxInfo = {}
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                self.lmList.append([id, cx, cy, cz])
+
+            # Bounding Box
+            ad = abs(self.lmList[12][1] - self.lmList[11][1]) // 2
+            if bboxWithHands:
+                x1 = self.lmList[16][1] - ad
+                x2 = self.lmList[15][1] + ad
+            else:
+                x1 = self.lmList[12][1] - ad
+                x2 = self.lmList[11][1] + ad
+
+            y2 = self.lmList[29][2] + ad
+            y1 = self.lmList[1][2] - ad
+            bbox = (x1, y1, x2 - x1, y2 - y1)
+            cx, cy = bbox[0] + (bbox[2] // 2), \
+                     bbox[1] + bbox[3] // 2
+
+            self.bboxInfo = {"bbox": bbox, "center": (cx, cy)}
+
+            if draw:
+                cv2.rectangle(img, bbox, (255, 0, 255), 3)
+                cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+        return self.lmList, self.bboxInfo
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+        """
+        Finds angle between three points. Inputs index values of landmarks
+        instead of the actual points.
+        :param img: Image to draw output on.
+        :param p1: Point1 - Index of Landmark 1.
+        :param p2: Point2 - Index of Landmark 2.
+        :param p3: Point3 - Index of Landmark 3.
+        :param draw:  Flag to draw the output on the image.
+        :return:
+        """
+
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        if angle < 0:
+            angle += 360
+
+        # Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return angle
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    def angleCheck(self, myAngle, targetAngle, addOn=20):
+        return targetAngle - addOn < myAngle < targetAngle + addOn
+
+
+def main():
+
+    # capture frames from a camera
+    cap = cv2.VideoCapture('resources/testVideos/test0.mp4')
+    detector = PoseDetector()
+    imPrev = None
+
+    curTime = time.time() # start time
+    fps = 0
+    lastXCenterDisplacement = 0
+
+    while True:
+
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False)
+
+        if bboxInfo:
+
+            # finding the center of the target pose
+            center = bboxInfo["center"]
+            cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+            # finding the difference between highest landmark and lowest landmark in pixels
+            yLocations = []
+            for lm in lmList :
+                yLocations.append(lm[2])
+            deltaY = max(yLocations) - min(yLocations)
+
+            occupiedHeight = deltaY / cv2.getWindowImageRect('img')[3] # target variable 1
+            xCenterDisplacement = (cv2.getWindowImageRect('img')[2] / 2) - center[0] # target variable 2
+            centerApproachSpeed = (xCenterDisplacement - lastXCenterDisplacement) * fps # target variable 3
+
+            lastXCenterDisplacement = xCenterDisplacement # displacement updation
+
+        # FPS calculation
+        fps = 1 / (time.time() - curTime)
+        curTime = time.time()
+        cv2.putText(img, '{0:.2f}'.format(fps), (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 1, cv2.LINE_AA)
+
+        # data output
+        print('--------------\n' + 'yBigness (%) = {0:.2f}\n'.format(occupiedHeight)
+              + 'Displacement from center (px) = {0:.2f}\n'.format(xCenterDisplacement)
+              + 'Speed of center approach (px/s) = {0:.2f}\n'.format(centerApproachSpeed)
+              + '--------------\n')
+
+        cv2.imshow("img", img)
+        imPrev = img
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
